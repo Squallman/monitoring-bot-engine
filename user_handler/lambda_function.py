5a2912cdd9c3c728d941084e574e211defcdcbbe
Shepel Serhii <serhii.shepel@agilelab.it>
2020-03-29 21:47:00 +0300

    Fixed DB_NAME property.

--- user_handler/lambda_function.py
+++ user_handler/lambda_function.py
@@ -11,14 +11,14 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT', 3306)
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_DBNAME = os.getenv('DB_DBNAME')
+DB_NAME = os.getenv('DB_NAME')
 
 DB = pymysql.connect(
     host=DB_HOST,
     port=3306,
     user=DB_USER,
     password=DB_PASSWORD,
-    database=DB_DBNAME
+    database=DB_NAME
 )
 
 
