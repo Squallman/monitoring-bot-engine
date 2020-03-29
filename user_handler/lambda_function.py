7b99e9ba42b9b5af57b2162d08cf45692a40e550
Shepel Serhii <serhii.shepel@agilelab.it>
2020-03-29 20:49:03 +0300

    Added commands to add and remove users.

--- user_handler/lambda_function.py
+++ user_handler/lambda_function.py
@@ -1,9 +1,35 @@
 import json
+import os
+import requests
+
+TELEGRAM_SEND_URL = 'https://api.telegram.org/bot%(token)s/sendMessage?chat_id=%(chat_id)s' \
+                    '&parse_mode=Markdown&text=%(message)s'
+
+TOKEN = os.getenv('TOKEN')
 
 
 def lambda_handler(event, context):
-    print(event)
+
+    if 'message' in event:
+        message = event.get('message')
+        text = message.get('text') if 'text' in message else ''
+        chat = message.get('chat') if 'chat' in message else ''
+        chat_id = chat.get('id') if 'id' in chat else ''
+        if 'start' in text.lower():
+            t_message = 'Looking for available slots was started'
+            telegram_send_message(TOKEN, chat_id, t_message)
+        elif 'stop' in text.lower():
+            t_message = 'Looking for available slots was stoped'
+            telegram_send_message(TOKEN, chat_id, t_message)
+        else:
+            t_message = 'Bot doesn\'t support this command, try "start" or "stop"'
+            telegram_send_message(TOKEN, chat_id, t_message)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+def telegram_send_message(token, chat_id, message):
+    params = {'token': token, 'chat_id': chat_id, 'message': message}
+    requests.get(url=TELEGRAM_SEND_URL % params)
